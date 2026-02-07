44c74a775c352eb44f5ff91b4e1292d5fb524a04
prajwalwankhade143 <prajwalwankhade202@gmail.com>
2026-02-07 18:04:46 +0530

    Ready for Streamlit Cloud deployment with Cloud MySQL

--- db_connection.py
+++ db_connection.py
@@ -1,9 +1,16 @@
 import mysql.connector
+import os
 
 def get_connection():
+    # Streamlit secrets (recommended)
+    host = os.getenv("DB_HOST", "YOUR_PLANETSCALE_HOST")
+    user = os.getenv("DB_USER", " root@localhost")
+    password = os.getenv("DB_PASS", "root123")
+    database = os.getenv("DB_NAME", "healthindia")
+
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root123",
-        database="healthindia"
+        host=host,
+        user=user,
+        password=password,
+        database=database
     )
