74fc94525bf4a04c308ac18649d3202d2f143951
prajwalwankhade143 <prajwalwankhade202@gmail.com>
2026-02-12 20:04:38 +0530

    Connected Railway MySQL database

--- db_connection.py
+++ db_connection.py
@@ -1,9 +1,17 @@
 import mysql.connector
+from urllib.parse import urlparse
+import os
+
+# Railway URL yaha paste karo
+DATABASE_URL = "mysql://root:QdnZxesGOAFEnZSFfdyvyRKTlEyAGBHV@metro.proxy.rlwy.net:18933/railway"
 
 def get_connection():
+    url = urlparse(DATABASE_URL)
+    
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root123",
-        database="healthindia"
+        host=url.hostname,
+        user=url.username,
+        password=url.password,
+        database=url.path[1:],   # removes leading '/'
+        port=url.port
     )
