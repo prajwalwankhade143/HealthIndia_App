b30f98ef7214274c9cbd630f1a92c7c621ad7b2b
prajwalwankhade143 <prajwalwankhade202@gmail.com>
2026-02-12 20:10:06 +0530

    Final Railway DB fix

--- db_connection.py
+++ db_connection.py
@@ -1,17 +1,17 @@
 import mysql.connector
 from urllib.parse import urlparse
-import os
-
-# Railway URL yaha paste karo
-DATABASE_URL = "mysql://root:QdnZxesGOAFEnZSFfdyvyRKTlEyAGBHV@metro.proxy.rlwy.net:18933/railway"
+import streamlit as st
 
 def get_connection():
+    DATABASE_URL = st.secrets["mysql://root:QdnZxesGOAFEnZSFfdyvyRKTlEyAGBHV@metro.proxy.rlwy.net:18933/railway"]
+
     url = urlparse(DATABASE_URL)
-    
+
     return mysql.connector.connect(
         host=url.hostname,
         user=url.username,
         password=url.password,
-        database=url.path[1:],   # removes leading '/'
-        port=url.port
+        database=url.path[1:],
+        port=url.port,
+        ssl_disabled=False
     )
