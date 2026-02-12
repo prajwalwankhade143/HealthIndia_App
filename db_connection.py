b6e7de7138beb9b1dd0cf044b5cf4c6d2b0c1b5d
prajwalwankhade143 <prajwalwankhade202@gmail.com>
2026-02-12 19:20:03 +0530

    Fixed MySQL connection issue and updated db configuration

--- db_connection.py
+++ db_connection.py
@@ -4,6 +4,6 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="YOUR_MYSQL_PASSWORD",
+        password="root123",
         database="healthindia"
     )
